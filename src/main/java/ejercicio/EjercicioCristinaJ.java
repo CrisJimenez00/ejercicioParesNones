2bcf9db9ef790e56e604d53624eac2701f1607a0
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-11-02 13:03:21 +0100

    Se crea el menú y el swtich

--- src/main/java/ejercicio/EjercicioCristinaJ.java
+++ src/main/java/ejercicio/EjercicioCristinaJ.java
@@ -4,6 +4,9 @@
  */
 package ejercicio;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author cristina
@@ -13,8 +16,32 @@ public class EjercicioCristinaJ {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {      
+        Random numeroRandom = new Random();
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Bienvenido al juego de pares y nones"
+                    + "\n1.Jugar con otro jugador en físico"
+                    + "\n2.Jugar contra la máquina"
+                    + "\n3.Salir"
+                    + "\nElija una opción correcta");
+            opcion = teclado.nextInt();
+        } while (opcion != 3);
+        switch(opcion){
+            case 1:
+                System.out.println("Ha elegido jugar contra persona en físico");
+                break;
+            case 2:
+                System.out.println("Ha elegido jugar contra la máquina");
+                break;
+            case 3:
+                System.out.println("Hasta la próxima");
+                break;
+            default:
+                System.out.println("Elija una opción correcta");
+
+        }
     }
     
 }
